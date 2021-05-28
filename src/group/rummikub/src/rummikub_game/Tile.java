9daa9a0284d8b24eebd41fd724a028b0872a550a
maik.tietz <maik.tietz@uni-konstanz.de>
2021-05-28 16:10:44 +0200

    update work tietz

--- src/group/rummikub/src/rummikub_game/Tile.java
+++ src/group/rummikub/src/rummikub_game/Tile.java
@@ -5,30 +5,47 @@ public class Tile {
     TileColor color;
     int value;
 
-    public Tile() { }
-
+    /**
+     * Constructor only for joker
+     * @param color of tile
+     */
     public Tile(TileColor color){
 
         this.color = color;
         this.value = 30;
     }
 
+    /**
+     * Constructor for any tile but joker
+     * @param color of tile
+     * @param value of tile
+     */
     public Tile(TileColor color, int value){
 
         this.color = color;
         this.value = value;
     }
 
+    /**
+     * @return color of tile
+     */
     public TileColor getTileColor() {
 
         return this.color;
     }
 
+    /**
+     * @return value of tile
+     */
     public int getValue() {
 
         return this.value;
     }
 
+    /**
+     * @return true if joker
+     * @return false if not joker
+     */
     public boolean isJoker(){
 
         return this.color == TileColor.JOKER;
