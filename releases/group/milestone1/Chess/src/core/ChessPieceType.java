c47557a1dedd8b841e9f19a1e36adec4eb89b60c
Jan de Boer <44832123+Janldeboer@users.noreply.github.com>
2021-06-02 10:56:15 +0200

    get ChessPieceType of char

--- releases/group/milestone1/Chess/src/core/ChessPieceType.java
+++ releases/group/milestone1/Chess/src/core/ChessPieceType.java
@@ -1,5 +1,24 @@
 package core;
 
 public enum ChessPieceType {
-    PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING, NONE
+    PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING;
+
+    public static ChessPieceType valueOf(char c) {
+        switch (c) {
+            case 'p','P':
+                return PAWN;
+            case 'n','N':
+                return KNIGHT;
+            case 'b','B':
+                return BISHOP;
+            case 'r','R':
+                return ROOK;
+            case 'q','Q':
+                return QUEEN;
+            case 'k','K':
+                return KING;
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
 }
