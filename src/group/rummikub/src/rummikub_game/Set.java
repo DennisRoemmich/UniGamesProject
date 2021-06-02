3ba56536b6ac9e0f5620f4b4d899f2a930fef4f3
Maria Fernanda Barrios Herrera <maria.barrios-herrera@uni-konstanz.de>
2021-06-02 11:29:19 +0200

    First steps Set

--- src/group/rummikub/src/rummikub_game/Set.java
+++ src/group/rummikub/src/rummikub_game/Set.java
@@ -13,7 +13,30 @@ public class Set {
     }
 
     public boolean isValid(){
+        boolean isRun = true;
+        boolean valid = false;
+        
+        if(isBigger(tiles[0], tiles[1]) && sameColor(tiles[0], tiles[1])){
+            int i = 1;
+            int j = 2;
+            while(isRun && j < tiles.length){
+                isRun = isBigger(tiles[i], tiles[j]) && sameColor(tiles[i], tiles[j]);
+            }
+        }
+
         return false;
+    }
+
+    public boolean sameColor(Tile a, Tile b){
+        return a.getTileColor() == b.getTileColor();
+    }
+
+    public boolean isBigger(Tile a, Tile b){
+        return a.getValue() > b.getValue();
+    }
+    
+    public boolean isSmaller(Tile a, Tile b){
+        return a.getValue() < b.getValue();
     }
 
     public SetType getType(){
