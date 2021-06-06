d486a08669409e51c20a340e9741743244096c37
Maria Fernanda Barrios Herrera <maria.barrios-herrera@uni-konstanz.de>
2021-06-06 19:15:10 +0200

    Fer Edit Board Class

--- src/group/rummikub/src/rummikub_game/Board.java
+++ src/group/rummikub/src/rummikub_game/Board.java
@@ -8,13 +8,18 @@ public class Board {
     public static final int GRID_WIDTH = 20;
 
     GridTile[][] grid = new GridTile[GRID_WIDTH][GRID_HEIGHT];
+    GridTile[][] empty = new GridTile[GRID_WIDTH][GRID_HEIGHT];
 
     public Board(){
 
     }
 
     public void addTile(Point position, Tile tile){
-
+        if(position.x < GRID_WIDTH && position.y < GRID_HEIGHT && grid[position.x][position.y].isEmpty()){
+            grid[position.x][position.y].setTile(tile);
+        } else {
+            System.out.println("invalid position");
+        }
     }
 
     public GridTile getGridTileAt(Point point){
@@ -22,11 +27,18 @@ public class Board {
     }
 
     public boolean moveTile(Point from, Point to){
-        return false;
+        if (from.x < GRID_WIDTH && from.y < GRID_HEIGHT && to.x < GRID_WIDTH && to.y < GRID_HEIGHT &&
+        grid[to.x][to.y].isEmpty()){
+            grid [to.x][to.y].setTile(grid[from.x][from.y].getTile());
+            grid[from.x][from.y].removeTile();
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public void clearBoard(){
-
+        grid = empty;
     }
 
     public boolean isValid(){
@@ -42,7 +54,6 @@ public class Board {
         return new Set[0];
     }
 
-
-
+}
 
 }
