989c2177544dbe14fd46c305a03d6c203296f1e5
maik.tietz <maik.tietz@uni-konstanz.de>
2021-06-07 17:10:53 +0200

    update rummikub tietz

--- src/group/rummikub/src/main/Main.java
+++ src/group/rummikub/src/main/Main.java
@@ -3,55 +3,44 @@ package main;
 import rummikub_game.Board;
 import rummikub_game.Rack;
 import rummikub_game.Rummikub;
-import rummikub_game.Tile;
 
 import java.awt.*;
 
 public class Main {
 
+    // create rummikub game
+
+    private static Rummikub rummi = new Rummikub(4, 0);
+
     public static void main(String[] args) {
-
-        // create rummikub game
-
-        var rummi = new Rummikub(4, 0);
 
         // testing
 
-        test(rummi);
+        test();
     }
 
-    private static void test(Rummikub rummi) {
+    private static void test() {
 
         // draw rack of players after handout
 
         for (var p = 0; p < 4; p++) {
             System.out.println("Rack of Player " + (p+1) + ":");
-            printPlayerRack(rummi, p);
+            printPlayerRack(p);
             newLine(1);
 
-/**     // draw each sorted players rack
+            // prints every players rack sorted by group and run
+        //    testSorting(p);
 
-        rummi.getPlayerAt(p).getRack().sortForGroup();
-        printPlayerRack(rummi, p);
-
-        rummi.getPlayerAt(p).getRack().sortForRun();
-        printPlayerRack(rummi, p);
-
-        newLine(1);
-        //**/
         }
 
-        // move a tile from current players sketchRack to sketchBoard position (4,4)
-
-        rummi.moveTileFromCurrentRackToBoard(new Point(0,0), new Point(4,4));
-        rummi.moveTileFromCurrentRackToBoard(new Point(0,1), new Point(4,5));
-        rummi.moveTileFromCurrentRackToBoard(new Point(0,2), new Point(4,6));
+        // moves tiles from current players sketchRack to sketchBoard position (4,4)
+    //    testTileMoving();
 
         newLine(1);
 
         // draw the board
 
-        printBoard(rummi);
+        printBoard();
 
         newLine(1);
 
@@ -60,11 +49,54 @@ public class Main {
         System.out.print("Is valid? : " + rummi.getSketchBoard().isValid());
         newLine(2);
 
-        printPlayerSketchRack(rummi, 0);
+        testMaik1();
+    }
+
+    private static void testSorting(int p) {
+
+        rummi.getPlayerAt(p).getRack().sortForGroup();
+        printPlayerRack(p);
+
+        rummi.getPlayerAt(p).getRack().sortForRun();
+        printPlayerRack(p);
+
+        newLine(1);
+    }
+
+    private static void testTileMoving() {
+
+        rummi.moveTileFromCurrentRackToBoard(new Point(0,0), new Point(4,4));
+        rummi.moveTileFromCurrentRackToBoard(new Point(0,1), new Point(4,5));
+        rummi.moveTileFromCurrentRackToBoard(new Point(0,2), new Point(4,6));
+
+        rummi.moveTileFromCurrentRackToBoard(new Point(0, 6), new Point(8, 3));
+    }
+
+    private static void testMaik1() {
+
+        printPlayerSketchRack(0);
 
         rummi.getPlayerAt(0).getSketchRack().sortForRun();
 
-        printPlayerSketchRack(rummi, 0);
+        printPlayerSketchRack(0);
+
+        if (rummi.finishMove()) {
+
+            printPlayerRack(0);
+            rummi.getPlayerAt(0).getRack().sortForRun();
+            printPlayerRack(0);
+        }
+
+        newLine(2);
+
+        printPlayerRack(1);
+
+        if (rummi.finishMove()) {
+
+            printPlayerRack(1);
+            rummi.getPlayerAt(1).getRack().sortForGroup();
+            printPlayerRack(1);
+        }
     }
 
     private static void newLine(int n) {
@@ -75,7 +107,7 @@ public class Main {
         }
     }
 
-    private static void printPlayerRack(Rummikub rummi, int player) {
+    private static void printPlayerRack(int player) {
 
         for (var i = 0; i < Rack.GRID_HEIGHT; i++) {
             for (var o = 0; o < Rack.GRID_WIDTH; o++) {
@@ -87,7 +119,7 @@ public class Main {
         newLine(1);
     }
 
-    private static void printPlayerSketchRack (Rummikub rummi,int player){
+    private static void printPlayerSketchRack (int player){
 
         for (var i = 0; i < Rack.GRID_HEIGHT; i++) {
             for (var o = 0; o < Rack.GRID_WIDTH; o++) {
@@ -99,7 +131,7 @@ public class Main {
         newLine(1);
     }
 
-    private static void printBoard (Rummikub rummi) {
+    private static void printBoard () {
 
         for(int i = 0; i < Board.GRID_HEIGHT; i++){
             for(int o = 0; o < Board.GRID_WIDTH; o++){
