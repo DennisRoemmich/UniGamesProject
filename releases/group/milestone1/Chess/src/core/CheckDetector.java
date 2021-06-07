d0800ce1a76e6ea657175c16f21e43cf62b8b0fc
Jan de Boer <44832123+Janldeboer@users.noreply.github.com>
2021-06-07 10:57:18 +0200

    Split inCheck method

--- releases/group/milestone1/Chess/src/core/CheckDetector.java
+++ releases/group/milestone1/Chess/src/core/CheckDetector.java
@@ -1,22 +1,33 @@
 package core;
 
+import core.pieces.ChessPiece;
 import core.pieces.ChessPieceType;
 import core.positioning.Square;
 
+import java.util.List;
+
 public class CheckDetector {
+
+    ChessBoard board;
 	
 	private CheckDetector() {
 		// Prevent initialization
 	}
 
-    public static boolean isInCheck(ChessBoard board, boolean color) {
-        Square kingPos = board.findPositionsOfPieces(ChessPieceType.KING, color).get(0);
-        for (Square opponentPos : board.findPositionsOfPieces(!color)) {
-            if(board.getPiece(opponentPos).findMovesDisregardingCheck(opponentPos, board).contains(kingPos)) {
+	public static boolean isSquareAttacked(ChessBoard board, Square squareToTest, boolean color) {
+        for (Square opponentSquare : board.findSquaresOfPieces(color)) {
+            ChessPiece opponentPiece = board.getPiece(opponentSquare);
+            List<Square> opponentCoveredSquares = opponentPiece.findCoveredSquares(board, opponentSquare);
+            if(opponentCoveredSquares.contains(squareToTest)) {
                 return true;
             }
         }
         return false;
+    }
+
+    public static boolean isInCheck(ChessBoard board, boolean color) {
+        Square kingSquare = board.findSquaresOfPieces(ChessPieceType.KING, color).get(0);
+        return isSquareAttacked(board, kingSquare, !color);
     }
 
     public static boolean isInCheckAfterMove(ChessBoard board, boolean color, Square origin, Square destination) {
