35d474c32e2d77b6e8168e2755f8fef12d67ae8b
Dennis Römmich <dennis.roemmich@uni-konstanz.de>
2021-06-08 15:58:14 +0200

    King.java refactored

--- releases/group/milestone1/Chess/src/core/pieces/King.java
+++ releases/group/milestone1/Chess/src/core/pieces/King.java
@@ -45,7 +45,6 @@ public class King extends CastlingChessPiece  {
         return moves;
     }
     
-    //TODO: Reduce Complexity 17 -> 15 and the for-Loop should include only one continue/break statement.
     private List<Square> findCastlingMoves(ChessBoard board) {
         List<Square> castlingMoves = new ArrayList<>();
         if (this.hasMoved() || CheckDetector.isInCheck(board, isWhite())) {
@@ -53,24 +52,22 @@ public class King extends CastlingChessPiece  {
         }
         Rank backRank = isWhite() ? Rank.M1 : Rank.M8;
         for (Square rookSquare : board.findSquaresOfPieces(ChessPieceType.ROOK, isWhite())) {
-            Rook rook = (Rook) board.getPiece(rookSquare);
-            if (rook.hasMoved()) {
-                continue;
-            }
-            Square kingSquare = new Square(backRank, File.E);
-            Direction kingMoveDirection = rookSquare.getFile() == File.A ? Direction.LEFT : Direction.RIGHT;
-            List<Square> kingMovementSquares = new ArrayList<>();
-            kingMovementSquares.add(kingSquare.getNext(kingMoveDirection));
-            kingMovementSquares.add(kingSquare.getNext(kingMoveDirection).getNext(kingMoveDirection));
-            for (Square square : kingMovementSquares) {
-                if (!board.isFieldFree(square) || CheckDetector.isSquareAttacked(board, square, !isWhite())) {
-                    continue;
-                }
-            }
-            if (kingMoveDirection == Direction.LEFT && !board.isFieldFree(new Square(backRank, File.B))) {
-                continue;
-            }
-            castlingMoves.add(kingSquare.getNext(kingMoveDirection).getNext(kingMoveDirection));
+            
+        	Rook rook = (Rook) board.getPiece(rookSquare);
+            
+            if (!rook.hasMoved()) {
+            	Square kingSquare = new Square(backRank, File.E);
+            	Direction kingMoveDirection = rookSquare.getFile() == File.A ? Direction.LEFT : Direction.RIGHT;
+            	List<Square> kingMovementSquares = new ArrayList<>();
+            	
+            	kingMovementSquares.add(kingSquare.getNext(kingMoveDirection));
+            	kingMovementSquares.add(kingSquare.getNext(kingMoveDirection).getNext(kingMoveDirection));
+            	
+            	if (kingMoveDirection == Direction.LEFT && !board.isFieldFree(new Square(backRank, File.B))) {
+            		continue;
+            	}
+            	castlingMoves.add(kingSquare.getNext(kingMoveDirection).getNext(kingMoveDirection));
+            } 
         }
         return  castlingMoves;
     }
