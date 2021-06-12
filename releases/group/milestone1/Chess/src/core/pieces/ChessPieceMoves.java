97da9410595116cf31e3aa53d478ed5350a1c4e8
Dennis Römmich <dennis.roemmich@uni-konstanz.de>
2021-06-12 20:20:01 +0200

    added jar + put into release folder

---
+++ releases/group/milestone1/Chess/src/core/pieces/ChessPieceMoves.java
@@ -0,0 +1,68 @@
+package core.pieces;
+
+import java.util.ArrayList;
+import java.util.List;
+import core.ChessBoard;
+import core.positioning.Direction;
+import core.positioning.Square;
+
+/**
+ * Game moves for the pices Queen, Bishop and rook.
+ * @author Jan de Boer, Dennis Roemmich
+ * 
+ */
+public class ChessPieceMoves {
+	
+	private boolean mBreakLoop = false;
+	private Square mOrigin;
+	private ChessPiece mPiece;
+	private ChessBoard mBoard;
+
+	public ChessPieceMoves(ChessPiece piece, Square square, ChessBoard board) {
+		this.mOrigin = square;
+		this.mPiece = piece;
+		this.mBoard = board;
+	}
+
+    private boolean isSquareReachable(Square squareToTest) {
+   	
+    	if (mBoard.isFieldFree(squareToTest)) {
+    		return true;
+    	} else {
+			mBreakLoop = true;
+    		return mBoard.isOccupiedByOpponent(squareToTest, mPiece.isWhite());
+    	}
+    }
+
+	public List<Square> getReachableSquares(Direction[] directions) {
+		List<Square> reachableSquares = new ArrayList<>();
+		for (Direction direction : directions) {
+			reachableSquares.addAll(getReachableSquares(direction));
+		}
+		return reachableSquares;
+	}
+
+	public List<Square> getReachableSquares(Direction direction) {
+		List<Square> lineSquares = new ArrayList<>();
+		Square squareToAdd = mOrigin.getNext(direction);
+		while (squareToAdd != null) {
+			lineSquares.add(squareToAdd);
+			squareToAdd = squareToAdd.getNext(direction);
+		}
+		return checkLine(lineSquares);
+	}
+
+	protected List<Square> checkLine(List<Square> lineSquares) {
+		List<Square> reachableSquares = new ArrayList<>();
+		for (Square square : lineSquares) {
+			if (isSquareReachable(square)) {
+				reachableSquares.add(square);
+			}
+			if (mBreakLoop) {
+				mBreakLoop = false;
+				break;
+			}
+		}
+		return reachableSquares;
+	}
+}
