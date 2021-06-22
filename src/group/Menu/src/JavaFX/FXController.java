33b73c04c30cc129089b2d93b3e1a3577431fde8
AK <andreas.kahabka@uni-konstanz.de>
2021-06-22 16:20:58 +0200

    fx stuff

---
+++ src/group/Menu/src/JavaFX/FXController.java
@@ -0,0 +1,13 @@
+package JavaFX;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+import java.awt.*;
+
+public class FXController {
+
+
+
+}
