b1af7dd811cbc9af24fe8134074f5a64a6c4e541
Jan de Boer <44832123+Janldeboer@users.noreply.github.com>
2021-06-26 14:43:49 +0200

    AIPlayer

--- src/milestone1/Chess/src/core/AiPlayer.java
+++ src/milestone1/Chess/src/core/AiPlayer.java
@@ -1,17 +1,30 @@
 package core;
 
+import console.ConsoleMain;
+import console.Controller;
 import framework.Player;
 import org.json.simple.JSONObject;
 
+import java.util.Optional;
+
 public class AiPlayer implements Player {
-    @Override
+
+	protected Controller mController;
+
+	public AiPlayer(Controller controller) {
+		this.mController = controller;
+	}
+
+	@Override
     public JSONObject requestMove(JSONObject dataType) {
 		if (dataType.get("type") != "move") {
 			return new JSONObject();
 		}
-
 		try {
 			//ChessMove move = ChessMove.valueOf(Ainput, mController);
+
+
+
 			//return move.toJSon();
 			return null;
 		} catch (Exception e) {
