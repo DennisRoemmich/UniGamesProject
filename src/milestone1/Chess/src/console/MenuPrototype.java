924947fef6c0fff9554879e96d08f8cc37ec09e1
Jan de Boer <44832123+Janldeboer@users.noreply.github.com>
2021-06-27 23:09:59 +0200

    Inital jar inclusion

--- src/milestone1/Chess/src/console/MenuPrototype.java
+++ src/milestone1/Chess/src/console/MenuPrototype.java
@@ -1,7 +1,10 @@
 package console;
 
+import TicTacToeFX.GUIStarter;
+import TicTacToeFernanda.TicTacToe;
 import framework.PrintToConsole;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class MenuPrototype {
@@ -24,6 +27,8 @@ public class MenuPrototype {
     private void printSelectGame() {
     	PrintToConsole.println("Please choose a game to play:");
     	PrintToConsole.println("[C]hess, [R]ummikub, [S]kat, Die Siedler von [K]onstanz, [Q]uit");
+    	PrintToConsole.printkn("Or play TicTacToe by entering \"T\" + the number of the team member (e.g. \"T1\"");
+        PrintToConsole.printkn("The inputs are not case-sensitive.");
     }
 
     private void handleGameInput() {
@@ -41,6 +46,16 @@ public class MenuPrototype {
                 break;
             case "K", "k":
             	PrintToConsole.println("This game isn't implemented yet :(");
+                break;
+            case "T1", "t1":
+                TicTacToe.main(new String[0]);
+                break;
+            case "T2", "t2":
+                break;
+            case "T3", "t3":
+                GUIStarter.main(new String[0]);
+                break;
+            case "T4", "t4":
                 break;
             case "Q", "q":
                 quitGame();
