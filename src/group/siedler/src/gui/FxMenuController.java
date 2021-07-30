195bc7ebbee2c2c7b36a1ef9c51e4a1cd4f7bd5f
Dennis Römmich <dennis.roemmich@uni-konstanz.de>
2021-07-30 21:03:09 +0200

    Version 1.0

---
+++ src/group/siedler/src/gui/FxMenuController.java
@@ -0,0 +1,133 @@
+package gui;
+
+import java.io.IOException;
+
+import controller.Main;
+import framework.PrintToConsole;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class FxMenuController {
+	
+	@FXML
+	protected Button mKonstanzButton;
+	@FXML
+	protected Button mStandardButton;
+	@FXML
+	protected TextField mFieldHumanAmount;
+	@FXML
+	protected TextField mFieldAiAmount;
+	@FXML
+	protected TextField mFieldStartMaterial;
+	
+	
+	ClassLoader mClassLoader;
+	
+    private static boolean mIsStandardMap;
+    
+    private static int mHumanAmount;
+    public static int getHumanAmount() {
+		return mHumanAmount;
+	}
+
+	public static void setHumanAmount(int humanAmount) {
+		FxMenuController.mHumanAmount = humanAmount;
+	}
+
+	private static int mAiAmount;
+    
+	public static int getAiAmount() {
+		return mAiAmount;
+	}
+
+	public static void setAiAmount(int aiAmount) {
+		FxMenuController.mAiAmount = aiAmount;
+	}
+
+	private static int StartingAmount;
+
+    public static int getStartingAmount() {
+		return StartingAmount;
+	}
+
+	public static void setStartingAmount(int startingAmount) {
+		FxMenuController.StartingAmount = startingAmount;
+	}
+
+	public static boolean ismIsStandardMap() {
+		return mIsStandardMap;
+	}
+
+	public static void setmIsStandardMap(boolean mIsStandardMap) {
+		FxMenuController.mIsStandardMap = mIsStandardMap;
+	}
+	
+
+	@FXML
+    public void handleStandardButton(ActionEvent event) {
+		setmIsStandardMap(true);
+    	
+		setHumanAmount(Integer.parseInt(mFieldHumanAmount.getText()));
+		setAiAmount(Integer.parseInt(mFieldAiAmount.getText()));
+		setStartingAmount(Integer.parseInt(mFieldStartMaterial.getText()));
+		
+    	mClassLoader = getClass().getClassLoader();
+        var resourceMain = mClassLoader.getResource("resources/SiedlerGUI.fxml");
+        if (resourceMain == null) {
+            PrintToConsole.println("\n=== URL is null ===\n");
+        }
+        FXMLLoader loaderMain = new FXMLLoader(resourceMain);
+        Parent rootMain;
+		try {
+			rootMain = loaderMain.load();
+	        Scene scene = new Scene(rootMain, 1200, 900);
+	        
+	        scene.getRoot().requestFocus();
+	        
+	    	Main.getPrimaryStage().setScene(scene);
+	    	Main.getPrimaryStage().setTitle("Die Siedler von Konstanz");
+	    	Main.getPrimaryStage().show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
+    }
+    
+    @FXML
+    public void handleKonstanzButton(ActionEvent event) {
+
+		try {
+	    	setmIsStandardMap(false);
+	    	
+			setHumanAmount(Integer.parseInt(mFieldHumanAmount.getText()));
+			setAiAmount(Integer.parseInt(mFieldAiAmount.getText()));
+			setStartingAmount(Integer.parseInt(mFieldStartMaterial.getText()));
+	    	
+	    	mClassLoader = getClass().getClassLoader();
+	        var resourceMain = mClassLoader.getResource("resources/SiedlerGUI.fxml");
+	        if (resourceMain == null) {
+	            PrintToConsole.println("\n=== URL is null ===\n");
+	        }
+	        FXMLLoader loaderMain = new FXMLLoader(resourceMain);
+	        Parent rootMain;
+			rootMain = loaderMain.load();
+	        Scene scene = new Scene(rootMain, 1200, 900);
+	    	
+	    	scene.getRoot().requestFocus();
+	    	
+	    	Main.getPrimaryStage().setScene(scene);
+	    	Main.getPrimaryStage().setTitle("Die Siedler von Konstanz");
+	    	Main.getPrimaryStage().show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
+    }
+}
